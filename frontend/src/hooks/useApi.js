1fa4efee67e9077b22f9f28f834eb8ac5c65023d
DamianDD1 <damiandelgadillodiaz@gmail.com>
2026-06-09 22:31:16 -0600

    Delete frontend directory

--- frontend/src/hooks/useApi.js
+++
@@ -1,9 +0,0 @@
-const BASE = import.meta.env.VITE_API_URL || 'http://localhost:3000/api'
-export const apiFetch = async (path, options = {}, token = null) => {
-  const headers = { 'Content-Type': 'application/json', ...options.headers }
-  if (token) headers['Authorization'] = `Bearer ${token}`
-  const res = await fetch(`${BASE}${path}`, { ...options, headers })
-  const data = await res.json()
-  if (!res.ok) throw new Error(data.error || 'Error')
-  return data
-}
